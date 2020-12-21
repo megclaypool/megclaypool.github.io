16c3a8278e432d348301c8cf1fb26f7972ae7148
Meg Claypool <margaret.claypool@gmail.com>
2020-12-21 12:57:48 -0800

    What about php files?

---
+++ javascriptmas.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>JavaScriptmas 2020</title>
+</head>
+<body>
+  <?php echo("Hello World in php!"); ?>
+</body>
+</html>
